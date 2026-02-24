a285719ed8de0627049502607d946f377993d720
Matheus Fontoura <matheusmf-s@hotmail.com>
2026-02-24 17:31:17 -0300

    creating controller and routes

--- techtest/config/routes.rb
+++ techtest/config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  get "books/index"
+  get "books/show"
+  get "books/create"
+  get "books/reserve"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  resources :books, only: [:index, :show, :create] do
+    post :reserve, on: :member
+  end
 
   # Defines the root path route ("/")
   # root "posts#index"
